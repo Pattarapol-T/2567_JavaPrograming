242390aec61672301de25857c94193ad0fae9853
Pattarapol-T <Pattarapol.theeraat@gmail.com>
2025-01-22 08:50:28 +0700

    Lab6Homework

---
+++ Labsheet06/src/TestIncomeCalculator.java
@@ -0,0 +1,9 @@
+class TestIncomeCalculator  {
+    public static void main(String[] args) {
+    IncomeCalculator taxCalculator = new IncomeCalculator();
+
+    taxCalculator.setIncome(250000);
+
+    taxCalculator.displayTaxDetails();
+    }
+}
